10e81d83ecd4004408adae80d29cc6b0ead4bdb4
gustavoBiasi <biasilima@gmail.com>
2016-12-04 13:31:57 -0200

    implementação google maps estatica

--- src/java/managedbeans/PropertyDetailManagedBean.java
+++ src/java/managedbeans/PropertyDetailManagedBean.java
@@ -5,36 +5,98 @@
  */
 package managedbeans;
 
+import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import model.Property;
+import org.primefaces.event.map.GeocodeEvent;
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.MapModel;
 import repository.PropertyRepository;
-
+import org.primefaces.model.map.GeocodeResult;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.Marker;
 /**
  *
  * @author Lara
  */
 @ManagedBean
-@RequestScoped
-public class PropertyDetailManagedBean {
 
+public class PropertyDetailManagedBean implements Serializable {
+
+    private MapModel geoModel;
+   
+    private String centerGeoMap = "41.850033, -87.6500523";
+    private String centerRevGeoMap = "41.850033, -87.6500523";
+
+    public MapModel getGeoModel() {
+        return geoModel;
+    }
+
+    public void setGeoModel(MapModel geoModel) {
+        this.geoModel = geoModel;
+    }
+
+    public String getCenterGeoMap() {
+        return centerGeoMap;
+    }
+
+    public void setCenterGeoMap(String centerGeoMap) {
+        this.centerGeoMap = centerGeoMap;
+    }
+
+    public String getCenterRevGeoMap() {
+        return centerRevGeoMap;
+    }
+
+    public void setCenterRevGeoMap(String centerRevGeoMap) {
+        this.centerRevGeoMap = centerRevGeoMap;
+    }
+     
+    @PostConstruct
+    public void init() {
+        geoModel = new DefaultMapModel();
+        property = propertyRepository.getById(1);
+       
+    }
+    
+    
+    
+    public void onGeocode(GeocodeEvent event) {
+           List<GeocodeResult> results = event.getResults();
+           System.out.println("baaaaaaaaala");
+        if (results != null && !results.isEmpty()) {
+            LatLng center = results.get(0).getLatLng();
+            centerGeoMap = center.getLat() + "," + center.getLng();
+                
+            for (int i = 0; i < results.size(); i++) {
+                GeocodeResult result = results.get(i);
+                result.setAddress(id);
+                geoModel.addOverlay(new Marker(result.getLatLng(), result.getAddress()));
+            }
+        }
+    }
     public Property property;
-    private PropertyRepository pr = new PropertyRepository();
+    private PropertyRepository propertyRepository = new PropertyRepository();
 
      public String id;
 
      public Property getProperty(){
          return this.property;
      }
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+            this.id = id;
+    }
 
-	public String detailAction() {
-	   //now action property contains "delete"
-           this.property=pr.getById(Integer.parseInt(id));
-           return "detail";
-	}
+    public String detailAction() {
+       //now action property contains "delete"
+       this.property= propertyRepository.getById(Integer.parseInt(id));
+       return "detail";
+    }
+    
+    
 }
